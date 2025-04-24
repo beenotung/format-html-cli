e3a5b34b10a5af41da32ae5c5541b435065de062
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-04-25 03:06:10 +0400

    init: setup cli project based on format-json-cli feat: finished case fixing for DOCTYPE changed by prettier

---
+++ README.md
@@ -0,0 +1,23 @@
+# format-html-cli
+
+Format html file as post-hook to fix prettier
+
+[![npm Package Version](https://img.shields.io/npm/v/format-html-cli.svg?maxAge=2592000)](https://www.npmjs.com/package/format-html-cli)
+
+## Usage
+
+1. Install `format-html-cli` as global or local package
+2. run `format-html-cli` from cli or npm script
+
+Details refer to [examples/package.json](./examples/package.json)
+
+## License
+
+This project is licensed with [BSD-2-Clause](./LICENSE)
+
+This is free, libre, and open-source software. It comes down to four essential freedoms [[ref]](https://seirdy.one/2021/01/27/whatsapp-and-the-domestication-of-users.html#fnref:2):
+
+- The freedom to run the program as you wish, for any purpose
+- The freedom to study how the program works, and change it so it does your computing as you wish
+- The freedom to redistribute copies so you can help others
+- The freedom to distribute copies of your modified versions to others
---
+++ cli.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+let fs = require('fs')
+let path = require('path')
+
+let skipFiles = {
+  'node_modules': '',
+  '.git': '',
+  'data': '',
+  'dist': '',
+  'build': '',
+  '.nyc_output': '',
+  'coverage': '',
+  '.angular': '',
+}
+
+function scanDir(dir) {
+  fs.readdirSync(dir).forEach(file => scanPath(dir, file))
+}
+
+function scanPath(dir, filename) {
+  if (filename in skipFiles) return
+  let file = path.join(dir, filename)
+  let stats = fs.lstatSync(file)
+  if (stats.isDirectory()) {
+    return scanDir(file)
+  }
+  if (stats.isFile()) {
+    return scanFile(file)
+  }
+}
+
+function scanFile(file) {
+  if (path.extname(file) !== '.html') return
+  let text = fs.readFileSync(file).toString()
+  let newText = text.replace(/^\<\!doctype html\>/i, `<!DOCTYPE html>`)
+
+  if (text === newText) {
+    console.log('skip file:', file)
+    return
+  }
+  fs.writeFileSync(file, newText)
+  console.log('saved file:', file)
+}
+
+scanDir('.')
